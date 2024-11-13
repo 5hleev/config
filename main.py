9c757db2c21622fcf74c7552eb91f2deb8e42155
5hleev <maksimshleev7@gmail.com>
2024-11-14 01:37:55 +0300

    Создание аргументов командной строки и функции main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-xdskibidi
+import argparse
+import os
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Shell Emulator")
+    parser.add_argument('--tar', required=True, help='Path to the tar file')
+    parser.add_argument('--log', required=True, help='Path to the log file')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    if not os.path.exists(args.tar):
+        print(f"Tar file {args.tar} does not exist")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
